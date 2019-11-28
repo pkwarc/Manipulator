b131209fffcaa655457ff2cf07d1c2bb583bd4ce
Piotr Kwarcinski <piotr.j.kwarcinski@gmail.com>
2019-11-28 21:45:19 +0100

    Add pin configuration.

--- src/main.c
+++ src/main.c
@@ -87,28 +87,73 @@ void send_string(char* s)
 
 
 void F1_start_clk() {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
 }
 
 
 void F1_start_ant() {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
 }
 
 
 void F1_stop() {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
 }
 
+void F2_start_clk() {
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
+}
+
+
+void F2_start_ant() {
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+
+void F2_stop() {
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_SET);
+}
+
+void U_start_clk() {
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+}
+
+
+void U_start_ant() {
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+
+void U_stop() {
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
+}
 
 void start_position()
 {
@@ -217,27 +262,51 @@ void EXTI15_10_IRQHandler()
 	EXTI->PR = EXTI_PR_PR13;
 }
 
+void EXTI0_IRQHandler()
+{
+	send_string("Manipulator movement start..\n");
+	man_start = 1;
+	EXTI->PR &= ~EXTI_PR_PR0;
+}
+
+void EXTI1_IRQHandler()
+{
+	send_string("Register movement start..\n");
+	EXTI->PR &= ~EXTI_PR_PR1;
+}
+
+void EXTI2_IRQHandler()
+{
+	send_string("CLOCKWISE! \n");
+	dir = CLOCKWISE;
+	EXTI->PR &= ~EXTI_PR_PR2;
+}
+
+void EXTI3_IRQHandler()
+{
+	send_string("ANTICLOCKWISE! \n");
+	dir = ANTICLOCKWISE;
+	EXTI->PR &= ~EXTI_PR_PR3;
+}
+
+void EXTI4_IRQHandler()
+{
+	send_string("Axis F1 Button..\n");
+	EXTI->PR &= ~EXTI_PR_PR4;
+}
 
 void EXTI9_5_IRQHandler()
 {
-	if (EXTI->PR & EXTI_PR_PR9)
+	if (EXTI->PR & EXTI_PR_PR5)
 	{
-		dir = CLOCKWISE;
-		send_string("CLOCKWISE! \n");
-	}
-	else if (EXTI->PR & EXTI_PR_PR8)
-	{
-		dir = ANTICLOCKWISE;
-		send_string("ANTICLOCKWISE! \n");
+		send_string("Axis F2 Button..\n")
 	}
 	else if (EXTI->PR & EXTI_PR_PR6)
 	{
-		send_string("START! \n");
-		man_start = 1;
-
+		send_string("Axis U Button..\n")
 	}
-	EXTI->PR &= ~EXTI_PR_PR9;
-	EXTI->PR &= ~EXTI_PR_PR8;
+
+	EXTI->PR &= ~EXTI_PR_PR5;
 	EXTI->PR &= ~EXTI_PR_PR6;
 }
 
@@ -285,7 +354,27 @@ void print_moves()
 // TIM3 PIN PA6 PA7
 // TIM4 PIN PB6 PB7
 
+// F1 PINS: PA10, PA11, PA12, PA13, PA14, PA15
+// F2 PINS: PC10, PC11, PC12, PC13, PC14, PC15
+// U  PINS: PB10, PB11, PB12, PB13, PB14, PB15
 
+// BUTTON S PB0
+// BUTTON T PB1
+// JOYSTICK FORWARD PB2
+// JOYSTICK BACKWARD PB3
+// AXIS F1 BUTTON PB4
+// AXIS F2 BUTTON PB5
+// AXIS U BUTTON PB6
+// DIOD F1 AXIS PC0
+// DIOD F2 AXIS PC1
+// DIOD U AXIS PC2
+
+// Possible states for S and T buttons:
+// S | T
+// 0 | 0 Manipulator off
+// 1 | 0 Manipulator movement
+// 1 | 1 For now no action
+// 0 | 1 Register movement
 
 int main(void)
 {
@@ -299,22 +388,62 @@ int main(void)
 	__HAL_RCC_GPIOD_CLK_ENABLE();
 	// CLOCK
 
-	// GPIO
 	GPIO_InitTypeDef gpio;
-	gpio.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8
-	    	| GPIO_PIN_9 | GPIO_PIN_10;
+
+	// GPIO A
+	gpio.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14
+	    	| GPIO_PIN_15;
 	gpio.Mode = GPIO_MODE_OUTPUT_PP;
 	gpio.Pull = GPIO_NOPULL;
 	gpio.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOA, &gpio);
-	// GPIO
+	// GPIO A
 
-	// BOARD BUTTON
-	gpio.Pin = GPIO_PIN_13;
-	gpio.Mode = GPIO_MODE_IT_RISING_FALLING;
+	// GPIO B
+		// output
+	gpio.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &gpio);
+		// input internal pull up no external resistors needed.
+	gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6;
+	gpio.Mode = GPIO_MODE_IT_FALLING;
 	gpio.Pull = GPIO_PULLUP;
+	HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+	HAL_GPIO_Init(GPIOB, &gpio);
+	// GPIO B
+
+
+	// GPIO C
+	gpio.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14
+	    	| GPIO_PIN_15;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOC, &gpio);
-	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+	// GPIO C
+
+	// GPIO D
+//	gpio.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14
+//	    	| GPIO_PIN_15;
+//	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+//	gpio.Pull = GPIO_NOPULL;
+//	gpio.Speed = GPIO_SPEED_FREQ_LOW;
+//	HAL_GPIO_Init(GPIOA, &gpio);
+	// GPIO D
+
+	// BOARD BUTTON UWAGA czy PC15 nie jest tylko do buttona?
+//	gpio.Pin = GPIO_PIN_13;
+//	gpio.Mode = GPIO_MODE_IT_RISING_FALLING;
+//	gpio.Pull = GPIO_PULLUP;
+//	HAL_GPIO_Init(GPIOC, &gpio);
+//	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 	// BOARD BUTTON
 
 	gpio.Pin = GPIO_PIN_9 | GPIO_PIN_8 | GPIO_PIN_6;
